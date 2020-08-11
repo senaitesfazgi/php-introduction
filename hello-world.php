33ffe0a51005b5c2f1c691628a73f0eaa2ec1ff7
senaitesfazgi <61923461+senaitesfazgi@users.noreply.github.com>
2020-08-11 13:24:01 -0600

    Addded different kinds of arrays

--- hello-world.php
+++ hello-world.php
@@ -27,3 +27,48 @@ echo "\nHello, my name is {$myObject->name} and my age is {$myObject->age} and I
 foreach ($myObject->hobbies as $hobby){
     echo "\n -  $hobby";
 }
+
+for ($iterator = 0; $iterator < 2; $iterator ++)
+{
+    echo "\n -  {$myObject->hobbies[$iterator]}";            
+}
+
+$myArrayLength = count($myArray);
+echo "\n\n The varibale 
+myArray contains {$myArrayLength} items";
+
+//While loop example
+?>While loop: <?php
+$myI = 10;
+
+while ($myI >= -35){
+    echo "\n While loop current vlaue  = $myI";
+    $myI -= 5;//Same as typing ...
+              //$myI = $myI - 5;
+}
+
+echo "\n\n Arrays again....\n\n";
+echo 'The first item in $mySecondArray is '. $myArray[0];
+echo "\n\nInstead of indexed arrays, we can also use associative arrays.\n That is to 
+say: arrays with key-value pairs!\n\n";
+$myAssociativeArray = array(
+    'name' => 'bob',
+    'age' => 41,
+    'Hobbies' => ["swimming", 'golf']//Indexed arrays have index nubers and values.
+);
+echo "{$myAssociativeArray['name']} is {$myAssociativeArray['age']}.";
+echo "\nHis second hobby is: {$myAssociativeArray['Hobbies'][1]}";
+
+// foreach in associative array
+foreach ($myAssociativeArray as $myKey => $myValue){
+    if (!is_array($myValue)){
+        echo "\n\nThe Associate value in {$myKey} is {$myValue} and his hobbies are";
+    }
+    else{
+        foreach($myValue as $hobbies){
+            echo "\n {$hobbies}";
+        }
+    }
+
+}
+
